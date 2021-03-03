2a4410d10f32c5b58d130394227f1bf69ec95ba7
dmsomov <somovdmitriy@icloud.com>
2021-03-03 19:21:37 +0300

    feat: add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import './Footer.css'
+import logo from '../components/img/img.png'
 
 class Footer extends Component {
   render() {
     return (
-      <footer>
-        FOOTER
+      <footer className='footer'>
+      <a href="https://rs.school/js/">
+        <img 
+          src={logo}
+          alt='logoRS' />
+      </a>
+      <a href="https://github.com/dmsomov/">Link Github</a>
+      <span>2021</span>
+        
       </footer>
     )
   }
