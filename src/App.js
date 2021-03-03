091fada6bb47af1555c9aa5fb3ba956754a3953f
dmsomov <somovdmitriy@icloud.com>
2021-03-03 18:29:53 +0300

    feat: add basic page layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Header from './components/Header'
+import Game from './components/Game'
+import Footer from './components/Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Game />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Game.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Game.css'
+
+class Game extends Component {
+  render() {
+    return (
+      <main className="game">
+        <h1>Content</h1>
+      </main>
+    )
+  }
+
+}
+
+export default Game;
